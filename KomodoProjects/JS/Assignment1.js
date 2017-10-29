4db68de8c738059d78afb2125aa473cdca72cc54
tafowler1 <33045747+tafowler1@users.noreply.github.com>
2017-10-29 18:14:44 -0500

    Add files via upload

--- KomodoProjects/JS/Assignment1.js
+++ KomodoProjects/JS/Assignment1.js
@@ -1,16 +1,16 @@
 
 function MenuChoice(selection)
 {
-    document.getElementById("Area1").style.visibility="hidden";
-    document.getElementById("Area2").style.visibility="hidden";
+    document.getElementById("Area1").style.display="none";
+    document.getElementById("Area2").style.display="none";
     
     switch(selection)
     {
         case "Area1":
-            document.getElementById("Area1").style.visibility="visible";
+            document.getElementById("Area1").style.display="initial";
             break;
         case "Area2":
-            document.getElementById("Area2").style.visibility="visible";
+            document.getElementById("Area2").style.display="initial";
             break;
         case "None":
             break;
